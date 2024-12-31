5c234ae4e78f61c25c79337436a7680408e4f494
MTruong2000 <truongdev2000@gmail.com>
2024-12-31 16:42:06 +0700

    create ui highl light your plan

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Checkout from './pages/checkout';
 import FormQuestion from './pages/formquestion';
+import HighlightYourPlan from './pages/highlightyourplan';
+
 
 function App() {
     return (
@@ -9,6 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<FormQuestion />} />
                     <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/highlight-your-plan" element={<HighlightYourPlan />} />
                 </Routes>
             </Router>
         </>
---
+++ src/pages/highlightyourplan/index.jsx
@@ -0,0 +1,67 @@
+import './style.scss';
+
+const HightLightYourPlan = () => {
+    return (
+        <>
+            <div className="block-highlight-your-plan">
+                <div className="block-highlight-your-plan-left">
+                    <div className="block-highlight-your-plan-left-title">Highlights of your plan</div>
+                    <div className="block-highlight-your-plan-left-desc">
+                        <div className="block-highlight-your-plan-left-desc-parent">
+                            <div className="block-highlight-your-plan-left-desc-parent-img">
+                                <img src="/icons/highlight-your-plan-icons-1.png" alt="img" />
+                            </div>
+                            <div className="block-highlight-your-plan-left-desc-parent-content">
+                                <strong>Easy and gentle workouts</strong>
+                                <p>to slim down and tone up easily at home</p>
+                            </div>
+                        </div>
+                        <div className="block-highlight-your-plan-left-desc-parent">
+                            <div className="block-highlight-your-plan-left-desc-parent-img">
+                                <img src="/icons/highlight-your-plan-icons-2.png" alt="img" />
+                            </div>
+                            <div className="block-highlight-your-plan-left-desc-parent-content">
+                                <strong>10-20 min routines</strong>
+                                <p>to increase strength and relieve back, neck and shoulder tension</p>
+                            </div>
+                        </div>
+                        <div className="block-highlight-your-plan-left-desc-parent">
+                            <div className="block-highlight-your-plan-left-desc-parent-img">
+                                <img src="/icons/highlight-your-plan-icons-3.png" alt="img" />
+                            </div>
+                            <div className="block-highlight-your-plan-left-desc-parent-content">
+                                <strong>Beginner-friendly exercises</strong>
+                                <p>to improve mobility and flexibility</p>
+                            </div>
+                        </div>
+                        <div className="block-highlight-your-plan-left-desc-parent">
+                            <div className="block-highlight-your-plan-left-desc-parent-img">
+                                <img src="/icons/highlight-your-plan-icons-4.png" alt="img" />
+                            </div>
+                            <div className="block-highlight-your-plan-left-desc-parent-content">
+                                <strong>Personalized meal plan</strong>
+                                <p>with over 1000 tasty recipes for losing weight</p>
+                            </div>
+                        </div>
+                        <div className="block-highlight-your-plan-left-desc-parent">
+                            <div className="block-highlight-your-plan-left-desc-parent-img">
+                                <img src="/icons/highlight-your-plan-icons-5.png" alt="img" />
+                            </div>
+                            <div className="block-highlight-your-plan-left-desc-parent-content">
+                                <strong>Expert tips and hacks</strong>
+                                <p>to improve your lifestyle and keep the weight off</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="block-highlight-your-plan-right">
+                    <div className="block-highlight-your-plan-right-img">
+                        <img src="/img/highlight-introduct.png" alt="img" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default HightLightYourPlan;
