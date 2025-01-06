5b734e2a8046df27b44aac2a42796a69d05fbeff
MTruong2000 <truongdev2000@gmail.com>
2025-01-06 14:23:38 +0700

    handle image

--- src/components/step/uiformstep6/index.jsx
+++ src/components/step/uiformstep6/index.jsx
@@ -5,35 +5,40 @@ const UIFormStep6 = ({ handleNextStep6 }) => {
     return (
         <>
             <div className="form-step-6">
-                <div>How many days a week do you want to exercise?</div>
-                <FormControl className="form-step-6__radio">
-                    <RadioGroup name="radio-buttons-group" onChange={handleNextStep6}>
-                        <FormControlLabel
-                            className="form-step-6__label"
-                            value="2 days"
-                            control={<Radio />}
-                            label="2 days"
-                        />
-                        <FormControlLabel
-                            className="form-step-6__label"
-                            value="3 days"
-                            control={<Radio />}
-                            label="3 days"
-                        />
-                        <FormControlLabel
-                            className="form-step-6__label"
-                            value="4 days"
-                            control={<Radio />}
-                            label="4 days"
-                        />
-                        <FormControlLabel
-                            className="form-step-6__label"
-                            value="5 or more days"
-                            control={<Radio />}
-                            label="5 or more days"
-                        />
-                    </RadioGroup>
-                </FormControl>
+                <div className='form-step-6-header'>How many days a week do you want to exercise?</div>
+                <div className="form-step-6-body">
+                    <div className="form-step-6-body-img">
+                        <img src="/img/form-step-6.png" alt="img" />
+                    </div>
+                    <FormControl className="form-step-6__radio">
+                        <RadioGroup name="radio-buttons-group" onChange={handleNextStep6}>
+                            <FormControlLabel
+                                className="form-step-6__label"
+                                value="2 days"
+                                control={<Radio />}
+                                label="2 days"
+                            />
+                            <FormControlLabel
+                                className="form-step-6__label"
+                                value="3 days"
+                                control={<Radio />}
+                                label="3 days"
+                            />
+                            <FormControlLabel
+                                className="form-step-6__label"
+                                value="4 days"
+                                control={<Radio />}
+                                label="4 days"
+                            />
+                            <FormControlLabel
+                                className="form-step-6__label"
+                                value="5 or more days"
+                                control={<Radio />}
+                                label="5 or more days"
+                            />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
             </div>
         </>
     );
